e50385e3c7e97da08e8e8d44aef3e761133c224f
Emmanuel Chagnas <emmanuelchagnas@gmail.com>
2014-10-11 07:50:18 +0200

    Creation Main and Screen1

---
+++ app/src/main/java/com/manu/activitiesnavigation/activity/Screen1Activity.java
@@ -0,0 +1,21 @@
+package com.manu.activitiesnavigation.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.manu.activitiesnavigation.R;
+
+/**
+ * Created by Manu on 11/10/2014.
+ */
+public class Screen1Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1);
+    }
+
+}
